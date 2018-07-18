46318c5157d7a194634ba0a584b80394d79e5106
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-07-17 21:57:33 -0400

    update to 2018

--- project.clj
+++ project.clj
@@ -3,16 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.typed "0.2.13"]
-                 [org.clojure/clojurescript "0.0-1859"]]
-  :plugins [[lein-typed "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.typed.cljs "0.5.4-SNAPSHOT"]
+                 [org.clojure/clojurescript "1.10.373"]]
+  :plugins [[lein-typed "0.4.6"]]
   :source-paths ["src/clj"
                  "src/cljs"]
-
   :core.typed {:check []
                :check-cljs [cljs-play.ann
                             #_cljs-play.dom
                             #_cljs-play.helpers
                             #_cljs-play.identity]}
-  :profiles {:dev {:repl-options {:port 64427}}})
+  :profiles {:dev {:dependencies [[cider/piggieback "0.3.6"]
+                                  [org.clojure/tools.nrepl "0.2.13"]]
+                   :repl-options {:port 64427
+                                  :nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}})
--- src/cljs/cljs_play/ann.cljs
+++ src/cljs/cljs_play/ann.cljs
@@ -1,5 +1,5 @@
 (ns cljs-play.ann
-  (:require-macros [cljs.core.typed :refer [ann check-ns]]))
+  (:require [cljs.core.typed :refer [ann check-ns]]))
 
 (ann foo number)
 (def foo 1)
